18c9d7c32a08f8ec31b608c631980a5e063d73c6
Daniil Korzh <49613644+Austral1a@users.noreply.github.com>
2019-08-22 04:33:28 +0300

    add Longest Word Results, coderyte; bubble sort

--- javascript.js
+++ javascript.js
@@ -418,3 +418,28 @@ const reverse = (input) => {
 
 //console.log( reverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) )
 //console.log(reverse('Hello, world!'))
+
+/* Longest Word Results, coderbyte */
+function LongestWord(sen) {
+    sen = sen.replace(/([^a-z])/gi, ' ');
+
+    return sen.split(' ').sort((a, b) => b.length - a.length)[0];
+}
+
+//console.log(LongestWord("a beautiful sentence^&!")) --> beautiful
+
+const bubbleSort = (arr) => {
+
+    for (let i = arr.length - 1; i > 0; i--) {
+
+        for (let j = 0; j < i; j++) {
+            jf(arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+    }
+    return arr;
+}
